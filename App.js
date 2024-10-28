ed5afb16d50f52cdb5ba2de79fe14d87634d66a2
jack-isnt-the-name-tafe <jack.brown3@studytafensw.edu.au>
2024-10-28 12:46:12 +1100

    Adds foundation for app navigation

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
+import React from 'react'
+import { View, Text, Button, StyleSheet } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from './screens/LoginScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import RootNavigator from './navigation/RootNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <LoginScreen />
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaProvider>
   );
 }
 
